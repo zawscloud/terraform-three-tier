7e46c072417e6251923be7a63c3640ecb696e1d3
Z. Jared Bleah <bleahzj@gmail.com>
2023-01-02 14:22:24 -0500

    Created sg

--- zawscloud-website-ecs/main.tf
+++ zawscloud-website-ecs/main.tf
@@ -32,3 +32,11 @@ module "nat_gateway" {
   private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
 
 }
+
+# create security groups
+
+module "security_group" {
+  source = "../modules/security-groups"
+  vpc_id = module.vpc.vpc_id
+
+}
