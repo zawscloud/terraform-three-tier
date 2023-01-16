68224fda8ea79ec0084b6a0d472f4668d3d4db1c
Z. Jared Bleah <bleahzj@gmail.com>
2023-01-16 18:34:02 -0500

    created ALB

--- zawscloud-website-ecs/main.tf
+++ zawscloud-website-ecs/main.tf
@@ -54,3 +54,16 @@ module "certmanager" {
   domain_name      = var.domain_name
   alternative_name = var.alternative_name
 }
+
+# Create Application Load Balancer
+
+module "application_load_balancer" {
+  source                = "../modules/alb"
+  project_name          = module.vpc.project_name
+  alb_security_group_id = module.security_group.alb_security_group_id
+  public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+  vpc_id                = module.vpc.vpc_id
+  certificate_arn       = module.certmanager.certificate_arn
+
+}
